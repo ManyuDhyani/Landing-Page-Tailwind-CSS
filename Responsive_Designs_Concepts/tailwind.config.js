34bec85fc04e77ec7e2d9e43a6b23ca9ea96b9a5
ManyuDhyani <manyudhyani@gmail.com>
2022-02-07 18:56:23 +0530

    Update: Responsive_Designs_Concepts

--- Responsive_Designs_Concepts/tailwind.config.js
+++ Responsive_Designs_Concepts/tailwind.config.js
@@ -2,6 +2,15 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {},
+    // screens: {
+    //   'slim': '400px',
+    //   // => @media (min-width: 400px) { ... }
+    //
+    //   'laptop': '1080px',
+    //   // => @media (min-width: 1080px) { ... }
+    // },
+    // But we have to define all breakpoints. 
+    // Once we define here even a single Breakpoint the inbuilt tailwind breakpoints will not work.
   },
   plugins: [],
 }
