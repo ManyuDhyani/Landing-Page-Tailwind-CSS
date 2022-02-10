da561601314e4f8df228f22287c8b02cee03f177
ManyuDhyani <manyudhyani@gmail.com>
2022-02-10 11:49:23 +0530

    updated index.html tailwind.config.js and added fullConf.js for Customizing Tailwind CSS using tailwind.config.js file Concept

--- Customizing_Tailwind_CSS_using_tailwind.config.js_file/tailwind.config.js
+++ Customizing_Tailwind_CSS_using_tailwind.config.js_file/tailwind.config.js
@@ -1,7 +1,35 @@
+//npx tailwindcss init 'fileName' --full
+//above command generate the full Config. file
+
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    // if i put any class like fontSize directly inside theme and not extend, i will overide the class.
+    screens: {
+      xsm: '500px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      // Can't add smaller breakpoints in screens in extend as the custom xsm will 
+      // come after all other pre-defined class and no effect will come to our 
+      // file if we use xsm, sm, md,etc togther. Only xsm effect will be there.
+      //  So add all in the theme direclty with custom breakpoints. But for larger 
+      // breakpoints we can add in extend. Means the breakpoints above custom breakpoints 
+      // (if any) will not work until its in theme.
+      // screens: {
+      //   xsm: '500px'
+      // },
+      spacing:{
+        13: '3.25rem' //we can also do 13: '13px', but to maintain consistency as other margin are in rem so I will use rem here too.
+      },
+      fontSize: {
+        '10xl': ['9rem', { lineHeight: '1.2' }],
+      }
+    },
   },
   plugins: [],
 }
